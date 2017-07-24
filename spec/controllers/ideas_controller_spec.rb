088a1c9efe5c3c6133dda98da277302fa44f562b
Ainkaran Pathmanathan <pat.ainkaran@gmail.com>
2017-07-24 15:02:04 -0700

    add RSpec changes

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe IdeasController, type: :controller do
 
+  let(:user) { FactoryGirl.create(:user) }
+  let(:idea)   { FactoryGirl.create(:idea, user: user) }
+  let(:idea_1) { FactoryGirl.create(:idea) }
+
   describe "#new" do
     context "with no signed in user" do
       it "redirects to the sign in page" do
@@ -26,11 +30,11 @@ RSpec.describe IdeasController, type: :controller do
       end
     end
   end
-
+=begin
   describe "#create" do
     context "user not signed in" do
       it "redirects to sign in page" do
-        attributes = FactoryGirl.attributes_for(:idea).merge(category_id: category.id, user_id: user.id)
+        attributes = FactoryGirl.attributes_for(:idea).merge(user_id: user.id)
         post :create, params: { idea: attributes }
         expect(response).to redirect_to new_session_path
       end
@@ -41,7 +45,7 @@ RSpec.describe IdeasController, type: :controller do
 
       context "with valid parameters" do
         def valid_request
-          attributes = FactoryGirl.attributes_for(:idea).merge(category_id: category.id, user_id: user.id)
+          attributes = FactoryGirl.attributes_for(:idea).merge(user_id: user.id)
           post :create, params: { idea: attributes }
         end
 
@@ -72,7 +76,7 @@ RSpec.describe IdeasController, type: :controller do
 
       context "with invalid parameters" do
         def invalid_request
-          attributes = FactoryGirl.attributes_for(:idea).merge(title: nil, category_id: category.id, user_id: user.id)
+          attributes = FactoryGirl.attributes_for(:idea).merge(title: nil, user_id: user.id)
           post :create, params: { idea: attributes }
         end
 
@@ -88,6 +92,63 @@ RSpec.describe IdeasController, type: :controller do
         it "sets a flash message" do
           invalid_request
           expect(flash[:alert]).to be
+        end
+      end
+    end
+  end
+=end
+
+  describe '#create' do
+    def valid_post
+      post :create, params: { idea: FactoryGirl.attributes_for(:idea) }
+    end
+
+    context 'with no user signed in' do
+      it 'redirects to the sign in page' do
+        valid_post
+        expect(response).to redirect_to(new_session_path)
+      end
+    end
+
+    context 'with signed in user' do
+      before do
+        # request.session[:user_id] = user.id
+        login(user)
+      end
+
+      context 'with valid attributes' do
+        it 'creates a new idea in the database' do
+          count_before = Idea.count
+          valid_post
+          count_after = Idea.count
+
+          expect(count_after).to eq(count_before + 1)
+        end
+
+        it 'redirects to the idea show page' do
+          valid_post
+          expect(response).to redirect_to(idea_path(Idea.last))
+        end
+
+        it 'associates the created idea with the signed in user' do
+          valid_post
+          expect(Idea.last.user).to eq(user)
+        end
+      end
+
+      context 'with invalid attributes' do
+        before do
+          idea = FactoryGirl.attributes_for(:idea)
+          idea[:title] = ''
+          post :create, params: { idea: idea }
+        end
+
+        it 'doesn\'t create a idea in the database' do
+          expect(Idea.count).to eq(0)
+        end
+
+        it 'renders new template' do
+          expect(response).to render_template(:new)
         end
       end
     end
--- spec/factories/ideas.rb
+++ spec/factories/ideas.rb
@@ -1,5 +1,16 @@
 FactoryGirl.define do
   factory :idea do
-    
+    # definining an association here will make so if you pass in a `:user`
+    # option to the campaign's factory, it will just use that. However, if you
+    # don't pass in a `:user` option, it will create a new user from scratch
+    # using th `user` factory
+    association :user, factory: :user
+
+    # To generate unique values for columns, use the sequence method.
+    # It takes a column (as symbol) for its argument and a block as argument
+    # as well. The block will be passed and incrementing for its argument.
+    sequence(:title) { |n| "#{Faker::Coffee.blend_name} #{n}" }
+    description { Faker::Coffee.notes }    
+    end_date { Faker::Date.between(1.month.from_now, 1.day.from_now) }
   end
 end
