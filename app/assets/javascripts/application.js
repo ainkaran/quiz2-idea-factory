7d44666bb5e7e8055a09cf2b480928a1d2bca8ef
Ainkaran Pathmanathan <pat.ainkaran@gmail.com>
2017-07-25 15:36:41 -0700

    add modal fixes-used bootstrap modal, re-structure shared folder template files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,24 @@ $(document).ready(function() {
 
   })
 })
+
+$(document).ready(function() {
+  $('new-idea-modal').on('click', function (event) {
+    const {currentTarget} = event;
+
+    $(currentTarget).parent().children('.idea-description').slideToggle( "slow" );
+
+  })
+})
+
+// $(document).ready(function() {
+//   $('#exampleModal').on('show.bs.modal', function (event) {
+//     var button = $(event.relatedTarget) // Button that triggered the modal
+//     var recipient = button.data('whatever') // Extract info from data-* attributes
+//     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+//     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//     var modal = $(this)
+//     modal.find('.modal-title').text('New message to ' + recipient)
+//     modal.find('.modal-body input').val(recipient)
+//   })
+// })
